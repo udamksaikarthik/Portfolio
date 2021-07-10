2109cf3885133f7906e5645f114a41ca60dd130f
udamksaikarthik <saikarthikjana@gmail.com>
2021-07-10 10:58:40 +0530

    Added all themes

--- JS/javascript.js
+++ JS/javascript.js
@@ -14,3 +14,35 @@ function darkgray(){
     DarkButton.classList.toggle("active");
     hamburger.classList.toggle("active");
 }
+
+const blueButton = document.getElementById("blueButton");
+
+function blueTheme(){
+    body.classList.toggle("blue");
+    blueButton.classList.toggle("active");
+    hamburger.classList.toggle("blue");
+}
+
+const pinkButton = document.getElementById("pinkButton");
+
+function pinkTheme(){
+    body.classList.toggle("pink");
+    pinkButton.classList.toggle("active");
+    hamburger.classList.toggle("pink");
+}
+
+const creamButton = document.getElementById("creamButton");
+
+function creamTheme(){
+    body.classList.toggle("cream");
+    creamButton.classList.toggle("active");
+    hamburger.classList.toggle("cream");
+}
+
+const lightwhiteButton = document.getElementById("lightwhiteButton");
+
+function lightwhiteTheme(){
+    body.classList.toggle("lightwhite");
+    lightwhiteButton.classList.toggle("active");
+    hamburger.classList.toggle("lightwhite");
+}
