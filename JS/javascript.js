02650c2f6eab6445c73312354dc2a6b65336c30f
udamksaikarthik <saikarthikjana@gmail.com>
2021-07-10 10:09:20 +0530

    Theme Button Added

--- JS/javascript.js
+++ JS/javascript.js
@@ -2,5 +2,15 @@ const headerItems = document.getElementById("nav");
 
 function showDropDown(){
     headerItems.classList.toggle("active");
+    hamburger.classList.toggle("act");
 }
 
+const body = document.getElementById("body");
+const DarkButton = document.getElementById("darkButton");
+const hamburger = document.getElementById("hamburger-button");
+
+function darkgray(){
+    body.classList.toggle("active");
+    DarkButton.classList.toggle("active");
+    hamburger.classList.toggle("active");
+}
